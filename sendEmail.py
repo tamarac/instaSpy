cbafeaea488d3d13a1edf8d17bae78e821c65c4d
Tamara Cavachioli <tamaracavachioli18@gmail.com>
2021-11-02 15:51:39 -0300

    refactor: alterando envio de email

--- sendEmail.py
+++ sendEmail.py
@@ -20,34 +20,34 @@ message["From"] = sender_email
 message["To"] = receiver_email
 
 def send(listFollowers, listFollowins, newFollowers, newFollowins, user):
-    followins = """ """
-    followers = """ """
-    html  = """
-    <html>
-    <body>
-        <p>Olá<br>
-        Há novas atualizações de """ + user + """:<br>"""
-    if newFollowins > 0:
-       followins = """Começou a seguir:""" + str(listFollowins) + """ <br>"""
-    elif newFollowins < 0:
-       followins = """Deixou de seguir:""" + str(listFollowins) + """ <br>"""
+   followins = """ """
+   followers = """ """
+   html  = """
+   <html>
+   <body>
+      <p>Olá<br>
+      Há novas atualizações de """ + user + """:<br>"""
+   if newFollowins > 0:
+      followins = """Começou a seguir:""" + str(listFollowins) + """ <br>"""
+   elif newFollowins < 0:
+      followins = """Deixou de seguir:""" + str(listFollowins) + """ <br>"""
     
-    if newFollowers > 0:
-       followers = """Começou a ser seguido por:""" + str(listFollowers) + """ </p>"""
-    elif newFollowers < 0:
-       followers = """Diexou de ser seguido por:""" + str(listFollowers) + """ </p>"""
+   if newFollowers > 0:
+      followers = """Começou a ser seguido por:""" + str(listFollowers) + """ </p>"""
+   elif newFollowers < 0:
+      followers = """Diexou de ser seguido por:""" + str(listFollowers) + """ </p>"""
 
-    fim = """</body>
-    </html>
-    """
-    completeHtml = html + followins + followers + fim
-    part2 = MIMEText(completeHtml, "html")
-    message.attach(part2)
+   fim = """</body>
+   </html>
+   """
+   completeHtml = html + followins + followers + fim
+   part2 = MIMEText(completeHtml, "html")
+   message.attach(part2)
    
-    if (newFollowers != 0 or newFollowins != 0):      
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp, port, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(
-                sender_email, receiver_email, message.as_string()
-            )
+   if (newFollowers != 0 or newFollowins != 0):
+      context = ssl.create_default_context()
+      with smtplib.SMTP_SSL(smtp, port, context=context) as server:
+         server.login(sender_email, password)
+         server.sendmail(
+            sender_email, receiver_email, message.as_string()
+         )
